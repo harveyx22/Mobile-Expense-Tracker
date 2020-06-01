217f7a582e86b68b1d4cf62b5415691001b5f1dc
barnotas <61534987+barnotas@users.noreply.github.com>
2020-05-31 23:00:06 -0700

    Added Budget class and budget entry content page

---
+++ Expense-Tracker-App/ExpenseTrackerApp/BudgetEntryPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using ExpenseTrackerApp.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExpenseTrackerApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BudgetEntryPage : ContentPage
+    {
+        public BudgetEntryPage()
+        {
+            InitializeComponent();
+        }
+         void OnSetBudgetClicked(object sender, EventArgs e)
+        {
+            var budget = (Budget)BindingContext;
+
+            if (string.IsNullOrWhiteSpace(budget.Filename))
+            {
+                // Set Budget 
+                var filename = Path.Combine(MainPage.FolderPath, $"{Path.GetRandomFileName()}.budgetes.txt");
+                File.WriteAllText(filename, budget.Text);
+            }
+            else
+            {
+                // Budget Is Set
+                File.WriteAllText(budget.Filename, budget.Text);
+            }
+        }
